28ca74c45e7d9ea9fafb9cc858371e3951930d66
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-13 16:58:14 -0800

    Finished

--- WindowShopper/WindowShopper/Views/CurrencyTxtField.swift
+++ WindowShopper/WindowShopper/Views/CurrencyTxtField.swift
@@ -10,6 +10,23 @@ import UIKit
 
 @IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.5901678396, green: 0.5964856789, blue: 0.6000595098, alpha: 0.6906571062)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formater = NumberFormatter()
+        formater.numberStyle = .currency
+        formater.locale = .current
+        currencyLbl.text = formater.currencySymbol
+        addSubview(currencyLbl)
+        
+    }
 
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -22,7 +39,8 @@ class CurrencyTxtField: UITextField {
             backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2472977312)
             layer.cornerRadius = 5.0
             textAlignment = .center
-            
+        
+            clipsToBounds = true
             if let p = placeholder {
                 let place = NSAttributedString(string: p,
                                                attributes: [.foregroundColor: UIColor.white])
